4b407af881d95426c32ecd5b4305a03cbe9ca467
jorgenmjobloop <jorgen@jobloop.no>
2024-09-02 10:57:14 +0200

    inital commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace KH_CS_September;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // string datatype variabel
+        string helloWorld = "Hello, World!";
+        // heltall variabel
+        int myNumber = 10;
+        // desimaltall/double variabel
+        double myDecimal = 10.25;
+        // bruker Console klassen til å skrive ut til terminalen
+        Console.WriteLine(helloWorld);
+        Console.WriteLine(myNumber);
+        Console.WriteLine(myDecimal);
+        
+        // Skriver ut en melding til brukeren 
+        Console.WriteLine("Hei, hva heter du? ");
+        // string variabel navn holder på verdien vi gir til konsollen
+        string? navn = Console.ReadLine();
+        // skriver ut en liten melding + navnet som er gitt til konsollen
+        Console.WriteLine("Hei " + navn);
+        // char typen, tar en bestemt bokstav
+        char a = '@';
+        Console.WriteLine(a);
+    }
+}
