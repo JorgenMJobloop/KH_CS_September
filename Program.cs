9f82e83702ee9a58773e90556ce96526cb7dce92
jorgenmjobloop <jorgen@jobloop.no>
2024-09-03 10:34:46 +0200

    added switch(case), arrays og for-loops

--- Program.cs
+++ Program.cs
@@ -22,9 +22,53 @@ class Program
         string? navn = Console.ReadLine();
         // skriver ut en liten melding + navnet som er gitt til konsollen
         Console.WriteLine("Hei " + navn);
-        // char typen, tar en bestemt bokstav
-        char a = '@';
-        Console.WriteLine(a);
-    }
 
+        // char typen, tar en bestemt bokstav
+        // char a = '@';
+        // Console.WriteLine(a);
+        /* vi lager et nytt array med å kalle på firkantparanteser
+           vi tildeler først en type, i dette tilfelle string etterfulgt av []
+           string[] <- string array, int[] <- int(heltall) array
+           og vi "populerer" arrayet med verdier inne i {} klammeparanteser.
+        */
+        /*
+        
+        */
+
+        string[] myStringArray = {"Hello", "C#", "Array!"};
+        for(int i = 0; i < myStringArray.Length; i = i + 1) 
+        {
+            Console.WriteLine(myStringArray[i]);
+        }
+
+        // List blir en datastruktur/klasse som "utvider" array. Vi kan legge til flere verdier i listen med
+        // Add() metoden f.eks: myNumbers.Add(4);
+        
+        // List<int> myNumbers = new List<int>();
+        // myNumbers.Add(1);
+        // myNumbers.Add(2);
+        // myNumbers.Add(3);
+        // myNumbers.Add(4);
+        // for(int i = 0; i < myNumbers.Count(); i++) 
+        // {
+        //     Console.WriteLine(myNumbers[i]);
+        // }
+
+        // switch(statement) case
+
+    
+        string? greetings = Console.ReadLine();
+        switch(greetings) 
+        {
+            case "Hei":
+                Console.WriteLine("Hei terminal!");
+                break;
+            case "hade":
+                Console.WriteLine("Hadebra terminal!");
+                break;
+            default:
+                Console.WriteLine("Alrighty");
+                break;
+        }
+    }
 }
